ff01e93ab127cb04f51c4e6217268cce621bcc5c
ArionDutta33 <ariondutta333@outlook.com>
2025-05-20 17:55:10 +0530

    Store selector

--- src/screens/StoreSelect.tsx
+++ src/screens/StoreSelect.tsx
@@ -4,24 +4,27 @@ import {
   ImageBackground,
   View,
   TouchableOpacity,
+  Platform,
 } from 'react-native';
 import React from 'react';
-import {SafeAreaView} from 'react-native-safe-area-context';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../types/navigation-types';
 import {useNavigation} from '@react-navigation/native';
 import Snackbar from 'react-native-snackbar';
+
 type LocalHomeProps = NativeStackNavigationProp<
   RootStackParamList,
   'StoreSelect'
 >;
+
 const StoreSelect = () => {
   const navigation = useNavigation<LocalHomeProps>();
+
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={styles.container}>
       {/* Fashion Store */}
       <TouchableOpacity
-        style={styles.imageContainer}
+        style={styles.card}
         onPress={() => {
           Snackbar.show({
             text: 'Site is under construction',
@@ -29,43 +32,43 @@ const StoreSelect = () => {
             action: {
               text: 'OK',
               textColor: 'green',
-              onPress: () => {
-                /* Do something. */
-              },
+              onPress: () => {},
             },
           });
         }}>
         <ImageBackground
-          style={styles.imageBackground}
+          imageStyle={styles.imageBackground}
+          style={styles.imageWrapper}
           source={{
-            uri: 'https://images.unsplash.com/photo-1558769132-cb1aea458c5e?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZmFzaGlvbnxlbnwwfHwwfHx8MA%3D%3D',
+            uri: 'https://images.unsplash.com/photo-1558769132-cb1aea458c5e?w=400&auto=format&fit=crop&q=60',
           }}>
           <View
             style={[styles.imageTextContainer, {backgroundColor: '#38a3a5'}]}>
-            <Text style={styles.fashionImageText}>Fashion Store</Text>
+            <Text style={styles.imageText}>Fashion Store</Text>
           </View>
         </ImageBackground>
       </TouchableOpacity>
 
       {/* Grocery Store */}
       <TouchableOpacity
-        style={styles.imageContainer}
+        style={styles.card}
         onPress={() => navigation.navigate('LocalHome')}>
         <ImageBackground
-          style={styles.imageBackground}
+          imageStyle={styles.imageBackground}
+          style={styles.imageWrapper}
           source={{
-            uri: 'https://images.unsplash.com/photo-1542838132-92c53300491e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z3JvY2VyeXxlbnwwfHwwfHx8MA%3D%3D',
+            uri: 'https://images.unsplash.com/photo-1542838132-92c53300491e?w=500&auto=format&fit=crop&q=60',
           }}>
           <View
             style={[styles.imageTextContainer, {backgroundColor: '#c8b6ff'}]}>
-            <Text style={styles.localShopStore}>Grocery Store</Text>
+            <Text style={styles.imageText}>Grocery Store</Text>
           </View>
         </ImageBackground>
       </TouchableOpacity>
 
       {/* Restaurant Store */}
       <TouchableOpacity
-        style={styles.imageContainer}
+        style={styles.card}
         onPress={() => {
           Snackbar.show({
             text: 'Site is under construction',
@@ -73,24 +76,23 @@ const StoreSelect = () => {
             action: {
               text: 'OK',
               textColor: 'green',
-              onPress: () => {
-                /* Do something. */
-              },
+              onPress: () => {},
             },
           });
         }}>
         <ImageBackground
-          style={styles.imageBackground}
+          imageStyle={styles.imageBackground}
+          style={styles.imageWrapper}
           source={{
-            uri: 'https://plus.unsplash.com/premium_photo-1661883237884-263e8de8869b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cmVzdGF1cmFudHxlbnwwfHwwfHx8MA%3D%3D',
+            uri: 'https://plus.unsplash.com/premium_photo-1661883237884-263e8de8869b?w=500&auto=format&fit=crop&q=60',
           }}>
           <View
             style={[styles.imageTextContainer, {backgroundColor: '#e63946'}]}>
-            <Text style={styles.restaurantStore}>Restaurant Store</Text>
+            <Text style={styles.imageText}>Restaurant Store</Text>
           </View>
         </ImageBackground>
       </TouchableOpacity>
-    </SafeAreaView>
+    </View>
   );
 };
 
@@ -99,28 +101,44 @@ export default StoreSelect;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 20,
+    gap: 20,
+    justifyContent: 'center',
+    backgroundColor: '#f7f7f7',
   },
-  fashionImageText: {
-    fontSize: 30,
-    color: 'white',
+  card: {
+    borderRadius: 15,
+    backgroundColor: '#fff',
+    overflow: Platform.OS === 'android' ? 'hidden' : 'visible',
+    ...Platform.select({
+      ios: {
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 6},
+        shadowOpacity: 0.25,
+        shadowRadius: 6,
+      },
+      android: {
+        elevation: 6,
+      },
+    }),
   },
-  localShopStore: {
-    fontSize: 30,
-    color: 'white',
-  },
-  restaurantStore: {
-    fontSize: 30,
-    color: 'white',
-  },
-  imageContainer: {
-    flex: 1,
+  imageWrapper: {
+    height: 200,
+    borderRadius: 15,
+    justifyContent: 'flex-end',
   },
   imageBackground: {
-    flex: 1,
-    alignItems: 'flex-end',
-    justifyContent: 'flex-end',
+    borderRadius: 15,
   },
   imageTextContainer: {
     padding: 10,
+    borderBottomLeftRadius: 15,
+    borderBottomRightRadius: 15,
+  },
+  imageText: {
+    fontFamily: 'Poppins-Bold',
+    fontSize: 24,
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
