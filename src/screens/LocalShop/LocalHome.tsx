63e3d4c7143be74c7272f76edc83ff268aef1c52
ArionDutta33 <ariondutta333@outlook.com>
2025-05-20 21:32:26 +0530

    Before Icons

--- src/screens/LocalShop/LocalHome.tsx
+++ src/screens/LocalShop/LocalHome.tsx
@@ -1,16 +1,23 @@
 import {StyleSheet, View} from 'react-native';
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
-
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faLocationDot} from '@fortawesome/free-solid-svg-icons';
 const LocalHome = () => {
   return (
-    <SafeAreaView>
-      <View style={styles.searchBar} />
+    <SafeAreaView style={styles.safeAreaContainer}>
+      <View style={styles.searchBar}>
+        <FontAwesomeIcon icon={faLocationDot} />
+      </View>
     </SafeAreaView>
   );
 };
 
 export default LocalHome;
 const styles = StyleSheet.create({
+  safeAreaContainer: {
+    flex: 1,
+    backgroundColor: '#d9d9d9',
+  },
   searchBar: {},
 });
